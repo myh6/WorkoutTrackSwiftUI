06b773e8d466583c8de794fb68b6f3b644d87957
myh <curryhuang3627@gmail.com>
2025-08-20 21:27:04 +0800

    Add `LocalizationTests`

--- WorkoutTrack/Package.swift
+++ WorkoutTrack/Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "WorkoutTrack",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15)
+        .iOS(.v18),
+        .macOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
