3fb9d91776387e95e7e442e661c6b36960077a14
myh <curryhuang3627@gmail.com>
2025-08-20 15:19:23 +0800

    Initial Commit

---
+++ WorkoutTrackSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutTrackSwiftUI
+//
+//  Created by Min-Yang Huang on 2025/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutTrackSwiftUI/WorkoutTrackSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackSwiftUIApp.swift
+//  WorkoutTrackSwiftUI
+//
+//  Created by Min-Yang Huang on 2025/8/20.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
