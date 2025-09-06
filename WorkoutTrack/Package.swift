62f6721ccfcd049377400983d3c8a69e42ff2043
Min Yang Huang <curryhuang3627@gmail.com>
2025-09-06 15:16:17 -0400

    Change swift-tools-version and platforms to build the Package

--- WorkoutTrack/Package.swift
+++ WorkoutTrack/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "WorkoutTrack",
     platforms: [
-        .iOS(.v18),
-        .macOS(.v15)
+        .iOS(.v17),
+        .macOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
