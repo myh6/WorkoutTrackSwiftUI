2148314e93c64f842bb098d8216f832a7d0bdba4
myh <curryhuang3627@gmail.com>
2025-09-06 14:45:59 -0400

    Create ExercisesLoader protocol

---
+++ WorkoutTrack/Tests/WorkoutTrackTests/ExercisesLoaderTests.swift
@@ -0,0 +1,27 @@
+//
+//  ExercisesLoaderTests.swift
+//  WorkoutTrack
+//
+//  Created by Min-Yang Huang on 2025/8/22.
+//
+
+import XCTest
+
+protocol ExercisesLoader {
+    func getAllExercises() -> [Any]
+}
+
+// 
+struct PresavedExercisesLoader {
+    
+}
+
+struct CustomSavedExercisesLoader {
+    
+}
+
+final class ExercisesLoaderTests: XCTestCase {
+    
+    
+
+}
