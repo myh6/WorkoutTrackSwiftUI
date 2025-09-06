36e555772e062c2659949ab85ae37c2f719ebfe4
Min Yang Huang <curryhuang3627@gmail.com>
2025-09-06 16:18:12 -0400

    Change swift-tools-version to 5.9 to avoid 6.0 API limitations

--- WorkoutTrack/Tests/WorkoutTrackTests/Helpers/XCTestCase+MemoryLeakTracking.swift
+++ WorkoutTrack/Tests/WorkoutTrackTests/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -8,9 +8,8 @@
 import XCTest
 
 extension XCTestCase {
-    @MainActor
     func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
-        addTeardownBlock {
+        addTeardownBlock { [weak instance] in
             XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
         }
     }
