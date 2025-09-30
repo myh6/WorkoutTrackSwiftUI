133f2a84d1d52026f34808ca45e42a5583e6175a
myh <curryhuang3627@gmail.com>
2025-09-30 16:08:42 -0400

    retrieve delivers empty on empty database

---
+++ WorkoutTrack/Tests/WorkoutTrackTests/Exercises Cache/SwiftDataExerciseStoreTests.swift
@@ -0,0 +1,49 @@
+//
+//  SwiftDataExerciseStoreTests.swift
+//  WorkoutTrack
+//
+//  Created by Min-Yang Huang on 2025/9/29.
+//
+
+import XCTest
+import WorkoutTrack
+import SwiftData
+
+@Model
+final class ExerciseEntity {
+    @Attribute(.unique) var id: UUID
+    var name: String
+    var category: String
+    
+    init(id: UUID, name: String, category: String) {
+        self.id = id
+        self.name = name
+        self.category = category
+    }
+}
+
+@ModelActor
+final actor SwiftDataExerciseStore {
+    func retrieve() throws -> [CustomExercise] {
+        return []
+    }
+}
+
+
+final class SwiftDataExerciseStoreTests: XCTestCase {
+    
+    func test_retrieve_deliversEmptyOnEmptyDatabase() async throws {
+        let sut = makeSUT()
+        let retrieved = try await sut.retrieve()
+        
+        XCTAssertEqual(retrieved, [])
+    }
+    
+    //MARK: - Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> SwiftDataExerciseStore {
+        let schema = Schema([ExerciseEntity.self])
+        let sut = try! SwiftDataExerciseStore(modelContainer: ModelContainer(for: schema, configurations: ModelConfiguration(isStoredInMemoryOnly: true)))
+        trackForMemoryLeaks(sut)
+        return sut
+    }
+}
