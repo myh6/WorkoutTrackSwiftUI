0b041eff1b59d757e6712648038b89a051e13544
myh <curryhuang3627@gmail.com>
2025-11-10 20:03:05 -0500

    `maxRepsPR` returns max reps from matching exercise that has finished

--- WorkoutTrack/Tests/WorkoutTrackTests/Statistics/PRCalculatorTests.swift
+++ WorkoutTrack/Tests/WorkoutTrackTests/Statistics/PRCalculatorTests.swift
@@ -14,25 +14,31 @@ struct ExerciseRecord {
     let set: WorkoutSetDTO
 }
 
-struct PRCalculator {
-    static func maxWeightPR(for exercise: UUID, from workouts: [WorkoutSessionDTO]) -> ExerciseRecord? {
-        let allSets: [ExerciseRecord] = workouts.flatMap { session in
+extension Array where Element == WorkoutSessionDTO {
+    func mapToExerciseRecords(for exercise: UUID) -> [ExerciseRecord] {
+        self.flatMap { session in
             session.entries
                 .filter { $0.exerciseID == exercise }
                 .flatMap { entry in
-                    entry.sets.map { set in
-                        ExerciseRecord(session: session, entry: entry, set: set)
-                    }
+                    entry.sets.map { ExerciseRecord(session: session, entry: entry, set: $0) }
                 }
         }
-        
-        return allSets
+    }
+}
+
+struct PRCalculator {
+    static func maxWeightPR(for exercise: UUID, from workouts: [WorkoutSessionDTO]) -> ExerciseRecord? {
+        return workouts
+            .mapToExerciseRecords(for: exercise)
             .filter { $0.set.isFinished }
             .max(by: { $0.set.weight < $1.set.weight })
     }
     
     static func maxRepsPR(for exercise: UUID, from workouts: [WorkoutSessionDTO]) -> ExerciseRecord? {
-        return nil
+        return workouts
+            .mapToExerciseRecords(for: exercise)
+            .filter { $0.set.isFinished }
+            .max(by: { $0.set.reps < $1.set.reps })
     }
 }
 
@@ -92,5 +98,25 @@ final class PRCalculatorTests: XCTestCase {
         let pr = PRCalculator.maxRepsPR(for: UUID(), from: workouts)
         
         XCTAssertNil(pr)
+    }
+    
+    func test_maxRepsPR_returnsMaxRepsFromMatchingExerciseThatHasFinished() throws {
+        let exerciseId = UUID()
+        let prSet = anySet(reps: 10, isFinished: true)
+        let prEntry = anyEntry(exercise: exerciseId, sets: [anySet(reps: 1, isFinished: true), prSet])
+        let prSession = anySession(entries: [prEntry])
+        let workouts = [
+            prSession,
+            anySession(entries: [
+                anyEntry(
+                    exercise: exerciseId,
+                    sets: [anySet(reps: 5, isFinished: false), anySet(reps: 40, isFinished: false)])])
+        ]
+        
+        let pr = try XCTUnwrap(PRCalculator.maxRepsPR(for: exerciseId, from: workouts))
+        
+        XCTAssertEqual(pr.session, prSession)
+        XCTAssertEqual(pr.entry, prEntry)
+        XCTAssertEqual(pr.set, prSet)
     }
 }
