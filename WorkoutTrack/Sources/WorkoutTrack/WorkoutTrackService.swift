f3fc734f457f7fd4dc655573e2bb61b27a63ee35
myh <curryhuang3627@gmail.com>
2025-11-19 16:57:17 -0500

    `addEntry` for deleted exercise should be skipped

---
+++ WorkoutTrack/Sources/WorkoutTrack/WorkoutTrackService.swift
@@ -0,0 +1,42 @@
+//
+//  WorkoutTrackService.swift
+//  WorkoutTrack
+//
+// Created by Min-Yang Huang on 2025/11/19
+//
+
+import Foundation
+
+class WorkoutTrackService {
+    private let exercise: ExerciseSystem
+    private let workoutTrack: WorkoutSessionStore
+    
+    init(exercise: ExerciseSystem, workoutTrack: WorkoutSessionStore) {
+        self.exercise = exercise
+        self.workoutTrack = workoutTrack
+    }
+}
+
+extension WorkoutTrackService {
+    func addCustomExercise(_ exercise: CustomExercise) async throws {
+        try await self.exercise.addExercise(exercise)
+    }
+    
+    func deleteExercise(_ exercise: CustomExercise) async throws {
+        try await self.exercise.removeExercise(exercise)
+    }
+}
+
+extension WorkoutTrackService {
+    func retrieveSessions(by query: SessionQueryDescriptor?) async throws -> [WorkoutSessionDTO] {
+        try await self.workoutTrack.retrieve(query: query)
+    }
+    
+    func addEntry(_ entries: [WorkoutEntryDTO], to session: WorkoutSessionDTO) async throws {
+        for entry in entries {
+            let existedExercise = try await exercise.loadExercises(by: .byID(entry.exerciseID))
+            guard !existedExercise.isEmpty else { return }
+            try await self.workoutTrack.insert([entry], to: session)
+        }
+    }
+}
--- WorkoutTrack/Tests/WorkoutTrackTests/WorkoutTrackIntegrationTests.swift
+++ WorkoutTrack/Tests/WorkoutTrackTests/WorkoutTrackIntegrationTests.swift
@@ -9,84 +9,56 @@ import XCTest
 @testable import WorkoutTrack
 import SwiftData
 
-class WorkoutTrackService {
-    private let exercise: ExerciseSystem
-    private let workoutTrack: WorkoutSessionStore
-    
-    init(exercise: ExerciseSystem, workoutTrack: WorkoutSessionStore) {
-        self.exercise = exercise
-        self.workoutTrack = workoutTrack
-    }
-}
-
-extension WorkoutTrackService {
-    func addCustomExercise(_ exercise: CustomExercise) async throws {
-        try await self.exercise.addExercise(exercise)
-    }
-}
-
-extension WorkoutTrackService {
-    func retrieveSessions(by query: SessionQueryDescriptor?) async throws -> [WorkoutSessionDTO] {
-        try await self.workoutTrack.retrieve(query: query)
-    }
-    
-    func addEntry(_ entries: [WorkoutEntryDTO], to session: WorkoutSessionDTO) async throws {
-        try await self.workoutTrack.insert(entries, to: session)
-    }
-}
-
 final class WorkoutTrackIntegrationTests: XCTestCase {
-    private var service: WorkoutTrackService!
-    
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        deleteStoreArtifacts()
-        let storeURL = testSpecificStoreURL()
-        let modelContainer = try makeTestModelContianer(storeURL: storeURL)
-        let workoutStore = SwiftDataWorkoutSessionStore(modelContainer: modelContainer)
-        let exerciseStore = SwiftDataExerciseStore(modelContainer: modelContainer)
-        let exerciseSystem = DefaultExerciseSystem(loaders: [PresavedExercisesLoader(), exerciseStore], inserter: exerciseStore, deleter: exerciseStore)
-        service = WorkoutTrackService(exercise: exerciseSystem, workoutTrack: workoutStore)
-    }
-    
-    override func tearDownWithError() throws {
-        try super.tearDownWithError()
-        deleteStoreArtifacts()
-    }
-    
-    //MARK: - Test Cases
+
     func test_addCustomExercise_andUseInWorkoutSession() async throws {
+        let sut = try makeSUT()
         let exercise = anyExercise(name: "Zercher Squat", category: .legs)
         let entry = anyEntry(exercise: exercise.id, sets: [anySet()])
         
-        try await service.addCustomExercise(exercise)
-        try await service.addEntry([entry], to: anySession())
+        try await sut.addCustomExercise(exercise)
+        try await sut.addEntry([entry], to: anySession())
         
-        let retrieved = try await service.retrieveSessions(by: nil).flatMap(\.entries)
+        let retrieved = try await sut.retrieveSessions(by: nil).flatMap(\.entries)
         XCTAssertEqual(retrieved.count, 1)
         let retrievedEntry = try XCTUnwrap(retrieved.first)
         XCTAssertEqual(retrievedEntry.exerciseID, exercise.id)
     }
     
+    func test_addEntry_forDeletedExercise_shouldBeSkipped() async throws {
+        let sut = try makeSUT()
+        let exercise = anyExercise()
+        
+        try await sut.addCustomExercise(exercise)
+        try await sut.deleteExercise(exercise)
+        try await sut.addEntry([anyEntry(exercise: exercise.id, sets: [anySet()])], to: anySession())
+        
+        let retrieved = try await sut.retrieveSessions(by: nil)
+        XCTAssertTrue(retrieved.isEmpty)
+    }
+    
     //MARK: - Helpers
-    private func makeTestModelContianer(storeURL: URL) throws -> ModelContainer {
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) throws -> WorkoutTrackService {
+        let modelContainer = try makeTestModelContianer()
+        let workoutStore = SwiftDataWorkoutSessionStore(modelContainer: modelContainer)
+        let exerciseStore = SwiftDataExerciseStore(modelContainer: modelContainer)
+        let exerciseSystem = DefaultExerciseSystem(
+            loaders: [PresavedExercisesLoader(), exerciseStore],
+            inserter: exerciseStore,
+            deleter: exerciseStore
+        )
+        let service = WorkoutTrackService(exercise: exerciseSystem, workoutTrack: workoutStore)
+        trackForMemoryLeaks(service, file: file, line: line)
+        return service
+    }
+    
+    
+    private func makeTestModelContianer() throws -> ModelContainer {
         let config = ModelConfiguration(
             "WorkoutTrackModel",
-            url: storeURL,
-            allowsSave: true)
+            isStoredInMemoryOnly: true
+            )
         return try ModelContainer(for: ExerciseEntity.self, WorkoutEntry.self, WorkoutSession.self, WorkoutSet.self, configurations: config)
-    }
-    
-    private func testSpecificStoreURL() -> URL {
-        FileManager.default
-            .urls(for: .cachesDirectory, in: .userDomainMask)
-            .first!
-            .appendingPathComponent("\(type(of: self)).store")
-    }
-    
-    private func deleteStoreArtifacts() {
-        let url = testSpecificStoreURL()
-        try? FileManager.default.removeItem(at: url)
     }
     
     private func anyExercise(id: UUID = UUID(), name: String = "any name", category: BodyCategory = .abs) -> CustomExercise {
